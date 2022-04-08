15206c7fe0e7fc34313a3159df1be4db79e44cfc
root <mulerabe@gmail.com>
2022-04-08 09:33:00 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: always 0(Success/Correct)
+*/
+int main(void) 
+
+{
+char number;
+for(number=48;number<=57; number++)
+{
+putchar(number);
+if (number ==57 ; number<=57; number++)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return(0);            
+}
