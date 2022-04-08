cef99158422147e91f52af820df670114a75c182
root <mulerabe@gmail.com>
2022-04-08 02:49:37 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main -Entry point
+*
+* Return: always 0(Success/Correct)
+*/
+void main()
+
+{
+char Lower_Case;
+char Upper_Case;
+
+for (Upper_Case= ‘A’; Upper_Case<= ‘Z’; Upper_Case++)
+{
+putchar(Upper_Case);
+}
+
+for (Lower_Case= ‘a’; Lower_Case<= ‘z’; Lower_Case++)
+{
+ putchar(Lower_Case);
+}
+putchar (‘\n’) ;
+return(0);            
+
+}
