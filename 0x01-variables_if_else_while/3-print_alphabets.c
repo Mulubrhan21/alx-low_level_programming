83ebb4514441a375b1cf1362cbbf4d625dd628e2
root <mulerabe@gmail.com>
2022-04-08 11:21:47 -0700

    edit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ void main()
 char Lower_Case;
 char Upper_Case;
 
- for (Lower_Case= ‘a’; Lower_Case<= ‘z’; Lower_Case++)
+ for (Lower_Case= 'a'; Lower_Case<= 'z'; Lower_Case++)
   {
  putchar(Lower_Case);
  }
-  for (Upper_Case= ‘A’; Upper_Case<= ‘Z’; Upper_Case++)
+  for (Upper_Case= 'A'; Upper_Case<= 'Z'; Upper_Case++)
   {
   putchar(Upper_Case);
   }         
