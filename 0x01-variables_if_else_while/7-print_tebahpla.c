54ee5979b772a029d38afdca3e224922cce4d50c
root <mulerabe@gmail.com>
2022-04-08 09:36:59 -0700

    editing

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -13,6 +13,6 @@ for(alphabet =122; alphabet >=97; alphabet--)
 {
 putchar(alphabet);
 }
-putchar('\n')
+putchar('\n');
 return(0);            
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,6 +15,6 @@ for(base_16 =97 ; base_16<=102; base_16++)
 {
 putchar(base_16);
 }
-putchar('\n')
+putchar('\n');
 return(0);            
 }
