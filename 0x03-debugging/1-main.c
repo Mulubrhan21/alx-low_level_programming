43332d89b7f38f2b2ce53a643fdc527248b8db13
root <mulerabe@gmail.com>
2022-04-13 22:13:57 -0700

    task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main -cause an infinte loop 
+*Return :0
+*/
+int main(void)
+{
+int i;
+printf("infinite loop incomming: (\n");
+i=0;
+/*
+*while(i<10)
+*{
+*Infinite loop - No increase of variable
+*putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\0/\n");
+return (0);
+}
