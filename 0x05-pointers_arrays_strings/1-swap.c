01efee7709c2372bb5721173fc29963c5f635084
root <mulerabe@gmail.com>
2022-04-18 11:02:02 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
-#include "main.h"
-/**
-*swap_int -swap the valueses of two integer a and b
-*@a: apointer to an int that will be updated
-*@b: B pointer to an int that will be updated
-*Return: void that means our answer is correct
-*/
-void Swap_int(int *a, int *b)
-{
-int f;
-f =0;
-f = *a;
-*a = *b;
-*b = f;
-}
+   #include "main.h"
+   /**
+   *swap_int -swap the valueses of two integer a and b
+   *@a: apointer to an int that will be updated
+   *@b: B pointer to an int that will be updated
+   *Return: void that means our answer is correct
+   */
+   void Swap_int(int *a, int *b)
+   {
+  int f;
+  f =0;
+  f = *a;
+  *a = *b;
+  *b = f;
+  }
+
