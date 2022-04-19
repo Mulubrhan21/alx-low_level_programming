800d227dd34ec00d41324e3220bd5b5a47c3a0ce
root <mulerabe@gmail.com>
2022-04-19 04:37:20 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -entry point
+*
+*Return : 0 after perinting the function
+*/
+int main(void)
+{
+puts("\"programming is like building a multiplation puzzzle");
+retur(0);
+}
