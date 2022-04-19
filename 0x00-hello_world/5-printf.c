7b7ce0e37bdf21c4c0c8feec8f5acc7206ce2e65
root <mulerabe@gmail.com>
2022-04-19 06:02:30 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main -entry point
+  *my hello, print task
+  *Return: return 1 after programe excuation
+  */
+int main(void)
+{
+	printf("with propor grammer but the outcome is apiece of art,\n");
+	return (0);
+}
