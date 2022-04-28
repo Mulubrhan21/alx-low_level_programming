5d3beebdebd059cc414640a4f5525e545498e216
root <mulerabe@gmail.com>
2022-04-28 00:03:05 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
-/*
-   *_putchar -writes character c to stdout
-   *return 0n success -1
-   */
-int _putchar(char c)
-{
-	return(write(1, &c, 1));
-}
+   /*
+      *_putchar -writes character c to stdout
+      *return 0n success -1
+      */
+   int _putchar(char c)
+   {
+           return(write(1, &c, 1));
+   }
