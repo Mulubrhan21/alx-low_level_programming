0e555b9797080982c5510c9a0764989af2bdbc4a
root <mulerabe@gmail.com>
2022-04-28 00:11:59 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,16 @@
-#include <unistd.h>
+#include "main.h"
    /*
       *_putchar -writes character c to stdout
       *return 0n success -1
       */
-   int _putchar(char c)
-   {
-           return(write(1, &c, 1));
-   }
+   int main(void)
+{
+	char mule[] ="_putchar";
+	int c;
+	for(c=0; c<8; c++)
+	{
+		_putchar(mule[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
